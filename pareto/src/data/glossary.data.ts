872ef0958c7456671e27609ef84e2e2dbf3a08ef
corno <corno.astn.a@schraverus.com>
2023-05-18 15:42:36 +0200

    .

--- pareto/src/data/glossary.data.ts
+++ pareto/src/data/glossary.data.ts
@@ -9,84 +9,80 @@ import * as g_glossary from "lib-pareto-typescript-project/dist/submodules/gloss
 const d = pd.d
 
 export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
-    'parameters': d({
+    'glossary parameters': d({
         "Annotation": null,
     }),
     'imports': d({
-        "h": imp({
-            "Annotation": glossaryParameter("Annotation")
-        }),
-        "schema": imp({
-            "Annotation": glossaryParameter("Annotation")
-        }),
+        "h": imp(),
+        "schema": imp(),
     }),
     'root': {
         'namespaces': d({}),
         'types': d({
             "Entry": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken")))),
+                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken", { "Annotation": glossaryParameter("Annotation") })))),
             })),
             "Group": type(group({
                 "type": member(taggedUnion({
-                    "verbose": ref(externalTypeReference("h", "OpenObjectToken")),
-                    "shorthand": ref(externalTypeReference("h", "OpenArrayToken")),
+                    "verbose": ref(externalTypeReference("h", "OpenObjectToken", { "Annotation": glossaryParameter("Annotation") })),
+                    "shorthand": ref(externalTypeReference("h", "OpenArrayToken", { "Annotation": glossaryParameter("Annotation") })),
                     "mixin": group({}),
                     "omitted": group({}),
                 })),
-                "definition": member(ref(externalTypeReference("schema", "group"))),
+                "definition": member(ref(externalTypeReference("schema", "group", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "List": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "OpenArrayToken")))),
-                "definition": member(ref(externalTypeReference("schema", "list"))),
+                "token": member(optional(ref(externalTypeReference("h", "OpenArrayToken", { "Annotation": glossaryParameter("Annotation") })))),
+                "definition": member(ref(externalTypeReference("schema", "list", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "ListClose": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "CloseArrayToken")))),
+                "token": member(optional(ref(externalTypeReference("h", "CloseArrayToken", { "Annotation": glossaryParameter("Annotation") })))),
             })),
             "Dictionary": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "OpenObjectToken")))),
-                "definition": member(ref(externalTypeReference("schema", "dictionary"))),
+                "token": member(optional(ref(externalTypeReference("h", "OpenObjectToken", { "Annotation": glossaryParameter("Annotation") })))),
+                "definition": member(ref(externalTypeReference("schema", "dictionary", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "DictionaryClose": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "CloseObjectToken")))),
+                "token": member(optional(ref(externalTypeReference("h", "CloseObjectToken", { "Annotation": glossaryParameter("Annotation") })))),
             })),
             "GroupClose": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "CloseObjectToken")))),
+                "token": member(optional(ref(externalTypeReference("h", "CloseObjectToken", { "Annotation": glossaryParameter("Annotation") })))),
             })),
             "TypeReference": type(group({
-                "definition": member(ref(externalTypeReference("schema", "type reference"))),
+                "definition": member(ref(externalTypeReference("schema", "type reference", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "TaggedUnion": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "TaggedUnionToken")))),
-                "definition": member(ref(externalTypeReference("schema", "tagged union"))),
+                "token": member(optional(ref(externalTypeReference("h", "TaggedUnionToken", { "Annotation": glossaryParameter("Annotation") })))),
+                "definition": member(ref(externalTypeReference("schema", "tagged union", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "SimpleString": type(group({
                 "value": member(string()),
-                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken")))),
-                "definition": member(ref(externalTypeReference("schema", "simple string"))),
+                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken", { "Annotation": glossaryParameter("Annotation") })))),
+                "definition": member(ref(externalTypeReference("schema", "simple string", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "MultilineString": type(group({
-                "token": member(optional(ref(externalTypeReference("h", "MultilineStringToken")))),
-                "definition": member(ref(externalTypeReference("schema", "multiline string"))),
+                "token": member(optional(ref(externalTypeReference("h", "MultilineStringToken", { "Annotation": glossaryParameter("Annotation") })))),
+                "definition": member(ref(externalTypeReference("schema", "multiline string", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "Option": type(group({
                 "name": member(string()),
-                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken")))),
-                "definition": member(ref(externalTypeReference("schema", "options"))),
+                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken", { "Annotation": glossaryParameter("Annotation") })))),
+                "definition": member(ref(externalTypeReference("schema", "options", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "UnexpectedOption": type(group({
                 "defaultOption": member(string()),//the unmarshaller will initialize the default option.
                 "expectedOptions": member(dictionary(null_())),
-                "token": member(ref(externalTypeReference("h", "SimpleStringToken"))),
+                "token": member(ref(externalTypeReference("h", "SimpleStringToken", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "Property": type(group({
                 "key": member(string()),
-                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken")))),//cannot be shorthand, so there must be a token, so not optional
-                "definition": member(ref(externalTypeReference("schema", "value"))),
+                "token": member(optional(ref(externalTypeReference("h", "SimpleStringToken", { "Annotation": glossaryParameter("Annotation") })))),//cannot be shorthand, so there must be a token, so not optional
+                "definition": member(ref(externalTypeReference("schema", "value", { "Annotation": glossaryParameter("Annotation") }))),
             })),
             "UnexpectedProperty": type(group({
                 "expectedProperties": member(dictionary(null_())),
-                "token": member(ref(externalTypeReference("h", "SimpleStringToken"))),
-                "groupDefinition": member(ref(externalTypeReference("schema", "group"))),
+                "token": member(ref(externalTypeReference("h", "SimpleStringToken", { "Annotation": glossaryParameter("Annotation") }))),
+                "groupDefinition": member(ref(externalTypeReference("schema", "group", { "Annotation": glossaryParameter("Annotation") }))),
             })),
         }),
     },
